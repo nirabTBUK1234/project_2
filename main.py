ee08929eb666a6c21811d4af33f081252cc4282c
nirabTBMK <nirabzzz14@gmail.com>
2026-01-08 17:00:01 +0545

    modified:   main.py 	new file:   media/videos/main/1080p60/partial_movie_files/SquarePro/260329756_2904653152_147125667.mp4 	modified:   media/videos/main/1080p60/partial_movie_files/SquarePro/partial_movie_file_list.txt

--- main.py
+++ main.py
@@ -9,20 +9,38 @@ import numpy as np
 
 class SquarePro(Scene):
     def construct(self):
+
+        # Counstruct SVG(Manim pic)
         img = SVGMobject("images.svg")
         Square1 = Square()
         self.play(Create(img),run_time=3)
         self.wait(1)
+
+        # Fade out Manim SVG
         self.play(FadeOut(img), run_time=3)
         self.wait(1)
-        axis = Axes(x_range=[-50, 60, 10],y_range=[-2.5, 2.5, 1],x_length=10,y_length=7,tips=True)
-        axis.add_coordinates()
-        self.add(axis)
-        graph = axis.plot(lambda x: np.sin(x / 10) ,x_range=[-50,60])
 
-        self.play(Create(axis))
+        # Create Axes
+        axes = Axes(
+            x_range=[-50, 60, 10],
+            y_range=[-2.5, 2.5, 1],
+            x_length=10,
+            y_length=7,
+            tips=True
+        ).add_coordinates()
+        
+        # Add Axes to the Scene(not animation)
+        self.add(axes)
+
+        # Create a sine wave 
+        graph = axes.plot(
+            lambda x: np.sin(x / 10),
+            x_range=[-50,60]
+        )
+
+        # Create the animation of sine wave graph and the Axes(actual animation)
+        self.play(Create(axes))
         self.wait(1)
-
         self.play(Create(graph))
 
 
