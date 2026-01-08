5277f4b523837518d3b8d9b41fa7d1d156ed2bb0
nirabTBMK <nirabzzz14@gmail.com>
2026-01-08 10:48:14 +0545

    modified:   main.py 	modified:   media/videos/main/1080p60/DoublePendulum.mp4 	new file:   media/videos/main/1080p60/SquarePro.mp4 	new file:   media/videos/main/1080p60/partial_movie_files/DoublePendulum/260329756_1839128356_601413116.mp4 	new file:   media/videos/main/1080p60/partial_movie_files/DoublePendulum/3544663784_513577100_223132457.mp4 	modified:   media/videos/main/1080p60/partial_movie_files/DoublePendulum/partial_movie_file_list.txt 	new file:   media/videos/main/1080p60/partial_movie_files/SquarePro/260329756_1839128356_601413116.mp4 	new file:   media/videos/main/1080p60/partial_movie_files/SquarePro/3544663784_513577100_223132457.mp4 	new file:   media/videos/main/1080p60/partial_movie_files/SquarePro/partial_movie_file_list.txt

--- main.py
+++ main.py
@@ -7,42 +7,8 @@ import numpy as np
 
 
 
-class DoublePendulum(Scene):
+class SquarePro(Scene):
     def construct(self):
-        # starting angles
-        θ1, θ2 = 2.0, 1.0
-        L1, L2 = 2.0, 1.5
-        origin = np.array([0,0,0])
-
-        def pendulum_points(theta1, theta2):
-            x1 = L1 * np.sin(theta1)
-            y1 = -L1 * np.cos(theta1)
-            x2 = x1 + L2 * np.sin(theta2)
-            y2 = y1 - L2 * np.cos(theta2)
-            return np.array([x1, y1, 0]), np.array([x2, y2, 0])
-
-        p1, p2 = pendulum_points(θ1, θ2)
-
-        bob1 = Dot(p1, color=BLUE)
-        bob2 = Dot(p2, color=GREEN)
-
-        rod1 = Line(origin, p1)
-        rod2 = Line(p1, p2)
-
-        self.add(rod1, rod2, bob1, bob2)
-
-        # animate a simple update loop
-        for i in range(60):
-            θ1 += 0.05
-            θ2 += 0.08
-            new_p1, new_p2 = pendulum_points(θ1, θ2)
-            self.play(
-                bob1.animate.move_to(new_p1),
-                bob2.animate.move_to(new_p2),
-                rod1.animate.put_start_and_end_on(origin, new_p1),
-                rod2.animate.put_start_and_end_on(new_p1, new_p2),
-                run_time=0.1,
-                rate_func=linear
-            )
-
+        Square1 = Square()
+        self.play(Create(Square1))
         self.wait(1)
